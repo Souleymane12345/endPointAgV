c53c1d8c8b66b6cf9990b08e7ffae858a8484f42
dabone <souleymanedabone123@gmail.com>
2022-06-15 13:07:16 +0000

    3 commit

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-var port = process.env.PORT
+//var port = process.env.PORT || 4500
 
-server.listen(port);
+server.listen(process.env.PORT || 5000);
 module.exports = app;
